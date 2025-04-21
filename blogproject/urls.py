ccb8372bf95b8a45f1c886e0092de2763c38d479
Javier Ayala <javier.ayala@utp.ac.pa>
2025-04-21 17:38:15 -0500

    Asegurando el URL de admin

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tulaperaconlabanana/', admin.site.urls),
     path('', include('blogapp.urls')),  # Conecta las URLs de blogapp
 ]
