07a817d734dc375e9b0572db23522d487a675b43
Ketzy Pitty <ketzy.pitty@utp.ac.pa>
2025-04-26 22:42:47 -0500

    Mejorando la configuración de Ckeditor

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blogapp'
 
@@ -10,5 +12,7 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
     path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
-    path('ckeditor/', include('ckeditor_uploader.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
